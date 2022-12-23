ba9a1b0f47d2b33d6431c7bd185e34674ebf2aac
Nurbek <nurbek.itu@gmail.com>
2022-12-23 14:42:15 +0600

    untracking .env

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,33 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { createGlobalStyle } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Global = createGlobalStyle`
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+  list-style: none;
+}
+
+body {
+	background-color: rgb(254, 248, 238);
+}
+
+a {
+	text-decoration: none;
+}
+`;
 
 root.render(
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
+          <Global />
           <App />
         </PersistGate>
       </Provider>
